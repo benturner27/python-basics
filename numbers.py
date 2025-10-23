2ea5f128caeca3308ea6e71d7b1a4885d2b62a3c
benturner27 <benturner27@proton.me>
2025-10-23 17:36:37 +0100

    Added numbers.py to the repository, showcasing the basic operators in base Python

---
+++ numbers.py
@@ -0,0 +1,33 @@
+#Basic mathematical operators
+
+#Addition
+a = 1 + 1
+print(a)
+
+#Subtraction
+b = 2 - 1
+print(b)
+
+#Multiplication
+c = 2 * 2
+print(c)
+
+#Division
+d = 4 / 2
+print(d)
+
+#Modulo
+e = 7 % 3
+print(e)
+
+#Power multiplication
+f = 2 ** 3
+print(f)
+
+#Order of operations
+g = 2 + 5 * 10 + 4
+print(g)
+
+#Order of operations (Brackets taking precedent)
+h = (2 + 5) * (10 + 4)
+print(h)
