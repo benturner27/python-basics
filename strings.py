11606a7efb3af4897c087778ff25a3fd7a8054f7
benturner27 <benturner27@proton.me>
2025-10-23 18:01:46 +0100

    Added strings.py to the repository, showing the basics of assigning strings, checking string lengths and using basic escape sequences

---
+++ strings.py
@@ -0,0 +1,24 @@
+#Strings
+#Are a sequence of characters that are inside single or double quotation marks
+a = 'hello'
+b = "world"
+c = "This is also a string"
+print(a)
+print(b)
+print(c)
+
+#When using apostrophes in strings, it's best to use double quotes. That way, there will be no syntax errors
+d = "I'm going on a run"
+print(d)
+
+#You can print strings either directly in the parameter for the print function, or through inputting a variable
+print("Hello, world!")
+
+#You can print strings on different lines using the "\n" escape sequence
+print("Hello,\nworld!")
+
+#You can also indent different parts of a string using the "\t" escape sequence
+print("Hello,\tworld!")
+
+#You can use the len function to check the length of a string
+print(len(c))
