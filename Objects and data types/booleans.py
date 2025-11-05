3f2116b46d18de25f2fa379740682a567d9a621a
benturner27 <benturner27@proton.me>
2025-11-05 23:53:53 +0000

    Added booleans.py and shared some basic examples

---
+++ Objects and data types/booleans.py
@@ -0,0 +1,16 @@
+#Booleans
+#Are operators that convey true or false statements
+#Important when dealing with control flow and logic
+#When defining booleans, true and false statements need to be capitalised
+
+#An example of a False statement
+a = 1 > 2
+print(a)
+
+#An example of a True statement
+b = 1 == 1
+print(b)
+
+#In Python, None is also available to use, as a way of a placeholder for a boolean.
+c = None
+print(c)
