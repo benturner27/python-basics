c2fca34609cea01fd29b3827523e26b651f48f48
benturner27 <benturner27@proton.me>
2025-11-30 22:25:28 +0000

    Added if_else_statements.py, added an example of an if statement

---
+++ 002 - Statements/if_else_statements.py
@@ -0,0 +1,22 @@
+#If-Elif-Else statements
+
+"""
+If, elif and else statements are flows of logic that execute pieces of code once a condition has been met. The main
+commands that are used are if (to indicate a certain condition), elif (if in case a different condition happens) and
+else (which executes code if a condition is not met).
+
+Flow syntax includes the use of colons and indentation.
+"""
+
+#Basic example of an if statement
+a = 3
+b = 2
+if a > b:
+    print("a is greater than b")
+elif a == b:
+    print("a is equal to b")
+else:
+    print("a is less than b")
+
+#You can add as many elif statements to the chain if different contitions are met
+
